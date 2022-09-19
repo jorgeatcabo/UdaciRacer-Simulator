e808b26e436d95609876c46f0aeebca06e028858
El Roble <san_lucas2005@yahoo.com>
2022-09-19 12:08:11 -0600

    Add getRace(id) and accelerate(id) methods

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -324,14 +324,14 @@ function getTracks() {
 	// GET request to `${SERVER}/api/tracks`
 	return fetch(`${SERVER}/api/tracks`)
 		.then(response => response.json())
-		.catch(error => console.log(error))
+		.catch(error => console.log("Problem with getTracks request::", err))
 }
 
 function getRacers() {
 	// GET request to `${SERVER}/api/cars`
 	return fetch(`${SERVER}/api/cars`)
 		.then(response => response.json())
-		.catch(error => console.log(error))
+		.catch(error => console.log("Problem with getRacers request::", err))
 }
 
 function createRace(player_id, track_id) {
@@ -351,6 +351,10 @@ function createRace(player_id, track_id) {
 
 function getRace(id) {
 	// GET request to `${SERVER}/api/races/${id}`
+	return fetch(`${SERVER}/api/races/${id}`)
+		.then(response => response.json())
+		.catch(error => console.log("Problem with getRace(id) request::", err))
+
 }
 
 function startRace(id) {
@@ -366,4 +370,10 @@ function accelerate(id) {
 	// POST request to `${SERVER}/api/races/${id}/accelerate`
 	// options parameter provided as defaultFetchOpts
 	// no body or datatype needed for this request
+	return fetch(`${SERVER}/api/races/${id}/accelerate`, {
+		method: 'POST',
+		...defaultFetchOpts(),
+	})
+		.then(res => res.json())
+		.catch(err => console.log("Problem with accelerate(id) request::", err))
 }
